03505251ecbe2ffe2ad1dbf88f31cf92937ef08f
GHMatrix <gsmensah@yahoo.com>
2024-08-15 08:41:53 +0000

    setting up engine for EJS

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// Set the engine for EJS
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
 //Import routes
 const indexRoutes = require('./src/routes/index');
 
